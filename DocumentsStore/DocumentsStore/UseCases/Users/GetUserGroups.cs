fb795d3bb8298930caa07e7e77392cef6fb3c150
Felipe Nader <fsnader@gmail.com>
2023-05-09 22:23:29 -0300

    Validate user in GetUserGroups

--- DocumentsStore/DocumentsStore/UseCases/Users/GetUserGroups.cs
+++ DocumentsStore/DocumentsStore/UseCases/Users/GetUserGroups.cs
@@ -7,14 +7,26 @@ namespace DocumentsStore.UseCases.Users;
 public class GetUserGroups : IGetUserGroups
 {
     private readonly IGroupUsersRepository _groupUsersRepository;
+    private readonly IUsersRepository _usersRepository;
 
-    public GetUserGroups(IGroupUsersRepository groupUsersRepository) => _groupUsersRepository = groupUsersRepository;
+    public GetUserGroups(IGroupUsersRepository groupUsersRepository, IUsersRepository usersRepository)
+    {
+        _groupUsersRepository = groupUsersRepository;
+        _usersRepository = usersRepository;
+    }
 
     public async Task<UseCaseResult<IEnumerable<Group>>> ExecuteAsync(int id, CancellationToken cancellationToken)
     {
         if (id <= 0)
         {
             return UseCaseResult<IEnumerable<Group>>.BadRequest("Please provide a valid id");
+        }
+
+        var user = await _usersRepository.GetByIdAsync(id, cancellationToken);
+
+        if (user is null)
+        {
+            return UseCaseResult<IEnumerable<Group>>.NotFound("User not found");
         }
         
         var results = await _groupUsersRepository.GetGroupsByUserIdAsync(id, cancellationToken);
