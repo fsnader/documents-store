4c54e5c52ac02fb6837f3970b623ed32d6b34200
Felipe Nader <fsnader@gmail.com>
2023-05-11 10:36:50 -0300

    Move getUser to ouside actions

--- DocumentsStore/DocumentsStore/Api/Controllers/DocumentsController.cs
+++ DocumentsStore/DocumentsStore/Api/Controllers/DocumentsController.cs
@@ -1,6 +1,7 @@
 using DocumentsStore.Api.DTOs;
 using DocumentsStore.Domain;
 using DocumentsStore.UseCases.Documents.Abstractions;
+using DocumentsStore.UseCases.Users.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DocumentsStore.Api.Controllers;
@@ -12,22 +13,29 @@ public class DocumentsController : BaseController
     private readonly ICreateDocument _createDocument;
     private readonly IGetDocumentById _getDocumentById;
     private readonly IGetUserAuthorizedDocuments _getUserAuthorizedDocuments;
-    
+    private readonly IGetUserById _getUserBy;
+
+    private async Task<User> GetCurrentUserAsync(CancellationToken cancellationToken)
+    {
+        var result = await _getUserBy.ExecuteAsync(1, cancellationToken);
+        return result.Result!;
+    }
+
     public DocumentsController(
-        ICreateDocument createDocument, 
-        IGetDocumentById getDocumentById, 
+        ICreateDocument createDocument,
+        IGetDocumentById getDocumentById,
         IGetUserAuthorizedDocuments getUserAuthorizedDocuments)
     {
         _createDocument = createDocument;
         _getDocumentById = getDocumentById;
         _getUserAuthorizedDocuments = getUserAuthorizedDocuments;
     }
-    
+
     [HttpPost]
     public async Task<IActionResult> Post([FromBody] CreateDocumentDto document, CancellationToken cancellationToken)
     {
         var result = await _createDocument.ExecuteAsync(
-            new User(),
+            await GetCurrentUserAsync(cancellationToken),
             document.ConvertToDocument(),
             document.AuthorizedUsers,
             document.AuthorizedGroups,
@@ -35,24 +43,30 @@ public class DocumentsController : BaseController
 
         return UseCaseActionResult(result, DocumentDto.CreateFromDocument);
     }
-    
-    
+
+
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(int id, CancellationToken cancellationToken)
     {
-        var result = await _getDocumentById.ExecuteAsync(new User(), id, cancellationToken);
-        
+        var result = await _getDocumentById.ExecuteAsync(
+            await GetCurrentUserAsync(cancellationToken),
+            id, cancellationToken);
+
         return UseCaseActionResult(result, DocumentDto.CreateFromDocument);
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetList(
         CancellationToken cancellationToken,
         [FromQuery] int take = 100,
         [FromQuery] int skip = 0)
     {
-        var result = await _getUserAuthorizedDocuments.ExecuteAsync(new User(), take, skip, cancellationToken);
-        
+        var result = await _getUserAuthorizedDocuments.ExecuteAsync(
+            await GetCurrentUserAsync(cancellationToken),
+            take,
+            skip,
+            cancellationToken);
+
         return UseCaseActionResult(result, DocumentDto.CreateFromDocuments);
     }
 }
