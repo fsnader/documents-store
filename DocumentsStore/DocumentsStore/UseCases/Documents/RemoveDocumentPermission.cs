dc5e8cc345e71726cc85fac84ca19d1d30e70010
Felipe Nader <fsnader@gmail.com>
2023-05-12 17:26:08 -0300

    Remove permissions

--- DocumentsStore/DocumentsStore/UseCases/Documents/RemoveDocumentPermission.cs
+++ DocumentsStore/DocumentsStore/UseCases/Documents/RemoveDocumentPermission.cs
@@ -1,13 +1,61 @@
 using DocumentsStore.Domain;
+using DocumentsStore.Repositories.Abstractions;
 using DocumentsStore.UseCases.Documents.Abstractions;
 
 namespace DocumentsStore.UseCases.Documents;
 
 public class RemoveDocumentPermission : IRemoveDocumentPermission
 {
-    public Task<UseCaseResult<Document>> ExecuteAsync(User user, int documentId, int permissionId, PermissionType type,
+    private readonly IDocumentsRepository _documentsRepository;
+
+    public RemoveDocumentPermission(IDocumentsRepository documentsRepository)
+    {
+        _documentsRepository = documentsRepository;
+    }
+
+    public async Task<UseCaseResult<Document>> ExecuteAsync(User user, int documentId, int permissionId,
+        PermissionType type,
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var documentTask = _documentsRepository.GetDocumentByIdAsync(documentId, cancellationToken);
+
+        var document = await documentTask;
+
+        if (document is null)
+        {
+            return UseCaseResult<Document>.NotFound();
+        }
+
+        if (document.UserId != user.Id)
+        {
+            return UseCaseResult<Document>.Unauthorized();
+        }
+        
+        await RemovePermissionAsync(documentId, permissionId, type, cancellationToken);
+
+        var authorizedUsers = _documentsRepository.GetDocumentUsersPermissionsAsync(documentId, cancellationToken);
+        var authorizedGroups = _documentsRepository.GetDocumentGroupsPermissionsAsync(documentId, cancellationToken);
+
+        document.AuthorizedUsers = await authorizedUsers;
+        document.AuthorizedGroups = await authorizedGroups;
+
+        return UseCaseResult<Document>.Success(document);
     }
+
+    private Task RemovePermissionAsync(int documentId, int permissionId, PermissionType type,
+        CancellationToken cancellationToken) =>
+        type switch
+        {
+            PermissionType.Group => _documentsRepository.RemoveDocumentGroupsPermissionAsync(
+                documentId,
+                permissionId,
+                cancellationToken),
+
+            PermissionType.User => _documentsRepository.RemoveDocumentUserPermissionAsync(
+                documentId,
+                permissionId,
+                cancellationToken),
+
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
 }
